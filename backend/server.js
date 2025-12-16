d4f59c6d69c125e7d5d8fed8d15b34ac66f8ed60
ansari5git <mohammadzaibansari5@gmail.com>
2025-12-16 11:39:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,20 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+const pool = require("./db");
+
+app.post("/order", async (req, res) => {
+  try {
+    const { name, email, phone, food, address } = req.body;
+
+    await pool.query(
+      "INSERT INTO orders (name, email, phone, food, address) VALUES ($1,$2,$3,$4,$5)",
+      [name, email, phone, food, address]
+    );
+
+    res.json({ success: true });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false });
+  }
+});
